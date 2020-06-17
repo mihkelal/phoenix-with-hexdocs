3356ddd29f71326c777ea3197dd4b63519a53834
mihkelal <mihkel.alavere@eesti.ee>
2020-06-17 21:22:37 +0300

    Add controller code

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -2,7 +2,10 @@ defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
   end
 
   def show(conn, %{"messenger" => messenger}) do
