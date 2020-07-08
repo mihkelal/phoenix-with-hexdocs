a404123ea75099fb52c53c62a599459e3a8a94e8
mihkelal <mihkel.alavere@eesti.ee>
2020-07-09 00:13:29 +0300

    Add some improvements

--- lib/hello_web/controllers/root_controller.ex
+++ lib/hello_web/controllers/root_controller.ex
@@ -2,6 +2,10 @@ defmodule HelloWeb.RootController do
   use HelloWeb, :controller
 
   def index(conn, _) do
-    redirect(conn, to: Routes.cms_page_path(conn, :index))
+    if Plug.Conn.get_session(conn, :user_id) do
+      redirect(conn, to: Routes.cms_page_path(conn, :index))
+    else
+      redirect(conn, to: Routes.session_path(conn, :new))
+    end
   end
 end
--- lib/hello_web/views/layout_view.ex
+++ lib/hello_web/views/layout_view.ex
@@ -1,3 +1,7 @@
 defmodule HelloWeb.LayoutView do
   use HelloWeb, :view
+
+  def logged_in?(conn) do
+    Map.has_key?(conn.assigns, :current_user)
+  end
 end
