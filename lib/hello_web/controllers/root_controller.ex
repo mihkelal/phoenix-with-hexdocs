c31d4653ea88582272ce403ecf5357b401627990
mihkelal <mihkel.alavere@eesti.ee>
2020-12-31 15:53:41 +0200

    Add channels code

--- lib/hello_web/controllers/root_controller.ex
+++ lib/hello_web/controllers/root_controller.ex
@@ -2,10 +2,6 @@ defmodule HelloWeb.RootController do
   use HelloWeb, :controller
 
   def index(conn, _) do
-    if Plug.Conn.get_session(conn, :user_id) do
-      redirect(conn, to: Routes.cms_page_path(conn, :index))
-    else
-      redirect(conn, to: Routes.session_path(conn, :new))
-    end
+    render(conn, "index.html")
   end
 end
